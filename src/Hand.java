b90c7f9579fee8f6d4dc0a6fa7e9c5fc273d6d2d
Cal Alexander <s1931801@ed.ac.uk>
2020-04-08 20:34:31 +0100

    add: basic Hand class that contains data on the hands state, and a getter and setter
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

--- src/Hand.java
+++ src/Hand.java
@@ -1,2 +1,23 @@
 public class Hand {
+
+    // Boolean instance field that stores whether the hand is open or not
+    public boolean isOpen = false;
+
+
+    /**
+     * Getter that returns the state of isOpen
+     * @return Boolean containing isOpen
+     */
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+
+    /**
+     * Setter that sets the state of isOpen
+     * @param open - Boolean containing the value that updates isOpen
+     */
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
 }
