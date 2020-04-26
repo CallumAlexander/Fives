4d50aa2a11ae0ada2e05b850e0cca62fcecab6f8
Cal Alexander <s1931801@ed.ac.uk>
2020-04-26 20:16:31 +0100

    add: AI class that inherits Player.java
    Signed-off-by: Cal Alexander <s1931801@ed.ac.uk>

---
+++ src/AI.java
@@ -0,0 +1,14 @@
+public class AI extends Player {
+
+
+    /**
+     * Constructor that adds one hand into the game when the round is counted out
+     *
+     * @param initialHand - Hand instance
+     */
+    public AI(Hand initialHand) {
+        super(initialHand);
+    }
+
+
+}
